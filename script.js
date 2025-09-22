30ae32aa7b22054921a1ed60b2e819b60d91d536
shubham somavamshi <shubhamraopatil123@gmail.com>
2025-09-22 20:41:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 //your code here
+const images = document.querySelectorAll('.image');
+
+    let dragSrc = null;
+
+   images.forEach(img =>{
+    img.addEventListener('dragstart',e=>{
+      dragSrc = img;
+      e.dataTransfer.effectAllowed = 'move';
+    })
+  
+     img.addEventListener('dragover', e => {
+    e.preventDefault(); 
+  });
+  img.addEventListener('drop', e => {
+    e.preventDefault();
+    if (dragSrc !== img) {
+      const parent = img.parentNode;
+      parent.insertBefore(dragSrc, img); 
+    }
+  });
+ })
